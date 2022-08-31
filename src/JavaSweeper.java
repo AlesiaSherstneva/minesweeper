ea82f0f05c49b20983180bfec1741f28c2f6dc62
Alesia Sherstneva <alesiasherstneva@gmail.com>
2022-08-31 10:48:09 +0300

    Lesson 1.3 - Create the constructor

--- src/JavaSweeper.java
+++ src/JavaSweeper.java
@@ -2,6 +2,15 @@ import javax.swing.*;
 
 public class JavaSweeper extends JFrame{
     public static void main(String[] args) {
-        new JavaSweeper().setVisible(true);
+        new JavaSweeper();
+    }
+
+    public JavaSweeper() {
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setTitle("Minesweeper");
+        setLocationRelativeTo(null);
+        setVisible(true);
+        setSize(300, 100);
+        setResizable(false);
     }
 }
