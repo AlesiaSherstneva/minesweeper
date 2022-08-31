48081365b9c74d8a31b0fceee98b5c4ab733dcfc
Alesia Sherstneva <alesiasherstneva@gmail.com>
2022-08-31 11:06:08 +0300

    Lesson 1.5 - Add panel

--- src/JavaSweeper.java
+++ src/JavaSweeper.java
@@ -1,16 +1,22 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class JavaSweeper extends JFrame{
+    JPanel panel;
+
     public static void main(String[] args) {
         new JavaSweeper();
     }
 
     public JavaSweeper() {
+        panel = new JPanel();
+        panel.setPreferredSize(new Dimension(500, 300));
+        add(panel);
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setTitle("Minesweeper");
-        setLocationRelativeTo(null);
         setVisible(true);
-        setSize(300, 100);
         setResizable(false);
+        pack();
+        setLocationRelativeTo(null);
     }
 }
