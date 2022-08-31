79cccae2bcb0b0b40dd073d219307fe1b9371b00
Alesia Sherstneva <alesiasherstneva@gmail.com>
2022-08-31 11:16:02 +0300

    Lesson 1.6 - Refactoring

--- src/JavaSweeper.java
+++ src/JavaSweeper.java
@@ -2,16 +2,24 @@ import javax.swing.*;
 import java.awt.*;
 
 public class JavaSweeper extends JFrame{
-    JPanel panel;
+    private JPanel panel;
 
     public static void main(String[] args) {
         new JavaSweeper();
     }
 
-    public JavaSweeper() {
+    private JavaSweeper() {
+        initPanel();
+        initFrame();
+    }
+
+    private void initPanel() {
         panel = new JPanel();
         panel.setPreferredSize(new Dimension(500, 300));
         add(panel);
+    }
+
+    private void initFrame() {
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setTitle("Minesweeper");
         setVisible(true);
