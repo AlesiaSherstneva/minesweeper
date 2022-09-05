b2d286d114e14475e40f245ba8b1f28a1a7fcd0c
Alesia Sherstneva <alesiasherstneva@gmail.com>
2022-09-05 22:00:39 +0300

    Lesson 2.4 - Show four pictures

--- src/JavaSweeper.java
+++ src/JavaSweeper.java
@@ -18,7 +18,10 @@ public class JavaSweeper extends JFrame {
             @Override
             protected void paintComponent(Graphics graphics) {
                 super.paintComponent(graphics);
-                graphics.drawImage(getImage(), 0, 0, this);
+                graphics.drawImage(getImage("bomb"), 0, 0, this);
+                graphics.drawImage(getImage("num1"), 50, 0, this);
+                graphics.drawImage(getImage("num1"), 50, 50, this);
+                graphics.drawImage(getImage("num1"), 0, 50, this);
             }
         };
         panel.setPreferredSize(new Dimension(500, 300));
@@ -34,8 +37,8 @@ public class JavaSweeper extends JFrame {
         setLocationRelativeTo(null);
     }
 
-    private Image getImage() {
-        ImageIcon icon = new ImageIcon("resources/image/bomb.png");
+    private Image getImage(String name) {
+        ImageIcon icon = new ImageIcon("resources/image/" + name + ".png");
         return icon.getImage();
     }
 }
